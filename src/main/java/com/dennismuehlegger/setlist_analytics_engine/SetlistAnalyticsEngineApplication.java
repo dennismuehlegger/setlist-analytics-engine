c7ad1f36e44e50326e9d512c5cae75e0eecb4b97
Dennis Mühlegger <muhleggerd@outlook.de>
2026-07-13 18:59:24 +0200

    initial project setup

---
+++ src/main/java/com/dennismuehlegger/setlist_analytics_engine/SetlistAnalyticsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.dennismuehlegger.setlist_analytics_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistAnalyticsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistAnalyticsEngineApplication.class, args);
+	}
+
+}
